c3d616bc57da54910bdd8d9e8b9030f706dbe4d9
Crowthecruel2 <fanie3vvuuren@gmail.com>
2024-05-15 00:02:41 +0200

    V0.01C

--- Darkest Timeline/World_Script.gd
+++ Darkest Timeline/World_Script.gd
@@ -1,15 +1,15 @@
 extends Node2D
 var Players
 var Teams
-
+@onready var Tile = preload("res://tile.tscn")
+@onready var Camera = $Camera2D
 var BoardSize
 var BoardX
 var BoardY
-var Tile_Counter
+var Tile_Counter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var Tile = preload("res://tile.tscn")
 	Players = get_tree().get_nodes_in_group("Players")
 	Teams = 2
 	BoardSize = Teams * 5
@@ -17,13 +17,18 @@ func _ready():
 		BoardSize = BoardSize-1
 	for x in BoardSize:
 		for y in BoardSize:
-			var NewTile = Tile.new()
-			NewTile.transform.origin = Vector2(x,y)
+			if((BoardSize-1)/2 == x && (BoardSize-1)/2 == y):
+				Camera.transform.origin = Vector2(x*75,y*75)
+			var NewTile = Tile.instantiate()
+			NewTile.transform.origin = Vector2(x*75,y*75)
 			Tile_Counter = Tile_Counter+1
 	print_debug(Tile_Counter)
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	
 	pass
